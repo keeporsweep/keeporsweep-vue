d9fed52737cdbf709271cbba06aeaa193e0d7af5
Jan-Christoph Borchardt <hey@jancborchardt.net>
2019-04-19 22:06:44 +0200

    Wrap in additional Vue component to fix Nextcloud CSP issue, ref https://blog.wuc.me/2018/10/18/nextcloud-vue-csp.html
    Signed-off-by: Jan-Christoph Borchardt <hey@jancborchardt.net>

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -27,7 +27,7 @@ module.exports = {
   output: {
     path: config.build.assetsRoot,
     filename: '[name].js',
-    library: 'errorfix',
+    jsonpFunction: 'webpackJsonpKeepOrSweep',
     publicPath: process.env.NODE_ENV === 'production'
       ? config.build.assetsPublicPath
       : config.dev.assetsPublicPath
---
+++ src/Main.vue
@@ -0,0 +1,48 @@
+<script>
+import Vue from 'vue';
+import shuffle from 'lodash.shuffle';
+import VueSwing from 'vue-swing';
+import App from './App';
+
+import amazon from './services/amazon';
+import email from './services/email';
+import github from './services/github';
+import home from './services/home';
+import instagram from './services/instagram';
+import netflix from './services/netflix';
+import twitter from './services/twitter';
+// import unsplash from './services/unsplash';
+
+Vue.component('vue-swing', VueSwing);
+
+export default {
+  name: 'Main',
+  components: { App },
+  template: '<App :elements="elements" />',
+  data() {
+    return {
+      elements: [],
+    };
+  },
+  methods: {},
+  created() {
+    // Add elements from services
+    this.elements = this.elements.concat(
+      amazon.elements,
+      email.elements,
+      github.elements,
+      home.elements,
+      instagram.elements,
+      netflix.elements,
+      twitter.elements,
+      // unsplash.elements,
+    );
+
+    // Randomize elements on startup
+    this.elements = shuffle(this.elements);
+    // this.elements = this.elements.sort(function() {
+    //   return 0.5 - Math.random();
+    // });
+  },
+};
+</script>
