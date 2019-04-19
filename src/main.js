0d0b42b34c2b570381dc2c4eff7baa6fc745cb43
Jan-Christoph Borchardt <hey@jancborchardt.net>
2019-04-19 22:55:21 +0200

    Add special case for Nextcloud CSP
    Signed-off-by: Jan-Christoph Borchardt <hey@jancborchardt.net>

--- src/main.js
+++ src/main.js
@@ -3,6 +3,19 @@
 import Vue from 'vue';
 import Main from './Main';
 
+// Special case for Nextcloud CSP
+// See https://blog.wuc.me/2018/10/18/nextcloud-vue-csp.html
+// And https://icewind.nl/entry/nextcloud-webpack/
+if (typeof OC !== 'undefined') {
+  // eslint-disable-next-line
+  __webpack_nonce__ = btoa(OC.requestToken);
+  // eslint-disable-next-line
+  __webpack_require__.p = OC.filePath('keeporsweep', 'js', '../js/');
+  const script = document.querySelector('[nonce]');
+  // eslint-disable-next-line
+  __webpack_require__.nc = script.nonce || script.getAttribute('nonce');
+}
+
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
